8e1c44d0e647444661308cdeda40fa4e63cd4f22
MatheoYaco240 <myacovino1@gmail.com>
2025-05-11 21:12:42 -0300

    finalizado

--- 00-CHelloWorld/hello.c
+++ 00-CHelloWorld/hello.c
@@ -3,10 +3,12 @@
 
 int main(){
     FILE *fp;
+    
     fp = fopen("output.txt", "w");
     if (fp == NULL) {
         return 1;
     }
+    // POR STDOUT --> fprintf(stdout, "Hello, World!\n");
     fprintf(fp, "Hello, World!\n");
     fclose(fp);
     return 0;
